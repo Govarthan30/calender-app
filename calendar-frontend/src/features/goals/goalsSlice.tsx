270d4a4dbdbe44542c80eb71421ed1990db1d263
govarthan v <231401030@rajalakshmi.edu.in>
2025-05-19 09:48:45 +0530

    Update goalsSlice.tsx

--- calendar-frontend/src/features/goals/goalsSlice.tsx
+++ calendar-frontend/src/features/goals/goalsSlice.tsx
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { Goal } from '../../types';
 
-const baseURL = 'http://localhost:5000';
+const baseURL = 'https://calender-app-9a73.onrender.com';
 
 export const fetchGoals = createAsyncThunk('goals/fetchGoals', async () => {
   const res = await axios.get<Goal[]>(`${baseURL}/goals`);
